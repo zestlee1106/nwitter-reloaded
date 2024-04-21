5fe17dcecccd07f5e928f3914d016af04e4eb56b
이영우 <zestlee1106@gmail.com>
2024-04-21 21:07:34 +0900

    firebase database 추가
    1. firebase 에서 firestore database 추가
    2. 만들었던 firebase 파일에 getFirestore 함수로 db 를 추가
    3. addDoc 에 해당 db 를 가져와서 collection 으로 추가함
    
    ----
    
    - table 이라는 개념이 firestore 에서는 collection 으로 불리우는 듯하다
    - noSQL 이기 때문에 하고 싶은대로 다 되는듯

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,7 @@
+import { addDoc, collection } from "firebase/firestore";
 import React, { useState } from "react";
 import styled from "styled-components";
+import { auth, db } from "../firebase";
 
 const Form = styled.form`
   display: flex;
@@ -73,9 +75,31 @@ export default function PostTweetForm() {
       setFile(files[0]);
     }
   };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+
+    if (!user || isLoading || tweet === "" || tweet.length > 180) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      await addDoc(collection(db, "tweets"), {
+        tweet,
+        createdAt: Date.now(),
+        username: user.displayName || "Anonymous",
+        userId: user.uid,
+      });
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <TextArea
         onChange={onChange}
         value={tweet}
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBpv4_r93laPfbSOUumjPJcsSDgfINzUU4",
@@ -13,3 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
