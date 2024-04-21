d9d0c7417970a605ab15c6abb9057403c2b30b30
이영우 <zestlee1106@gmail.com>
2024-04-21 14:38:26 +0900

    firebase get auth 연동
    1. firebase 에서 auth 사용 enable
    2. firebase 스크립트에서 getAuth 를 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { useEffect, useState } from "react";
 import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
 
 const router = createBrowserRouter([
   {
@@ -49,10 +50,8 @@ body {
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const init = async () => {
-    // wait for firebase
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
+    await auth.authStateReady();
+    setIsLoading(false);
   };
   useEffect(() => {
     init();
