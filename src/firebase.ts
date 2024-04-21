c0ab40561637ce627514bab2cfb26ff5715cd2c1
이영우 <zestlee1106@gmail.com>
2024-04-21 14:29:53 +0900

    firebase 추가

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpv4_r93laPfbSOUumjPJcsSDgfINzUU4",
+  authDomain: "nwitter-reloaded-f77bd.firebaseapp.com",
+  projectId: "nwitter-reloaded-f77bd",
+  storageBucket: "nwitter-reloaded-f77bd.appspot.com",
+  messagingSenderId: "147514274859",
+  appId: "1:147514274859:web:e6f427ae0bc4074117e66e",
+};
+
+const app = initializeApp(firebaseConfig);
