5e94284635812840954fde944dc76c75795da9b0
이영우 <zestlee1106@gmail.com>
2024-04-21 15:03:00 +0900

    firebase 로 create user 추가
    - firebase 에 이미 많은 api 들이 있음.. 가입 후 응답으로 user 를 줌
    - react router dom 에서 제공하는 useNavigate 로 페이지를 이동함 (router.push 같은 기능임)

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,8 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import styled from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
   height: 100%;
@@ -42,6 +45,7 @@ const Error = styled.span`
 `;
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -66,13 +70,27 @@ export default function CreateAccount() {
     }
   };
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    if (isLoading || name === "" || password === "" || email === "") {
+      return;
+    }
+
     try {
-      // create an account
-      // set the name of the user
-      // redirect to the home page
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(credentials.user);
+
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+
+      navigate("/");
     } catch (e) {
       // setError
     } finally {
@@ -83,7 +101,7 @@ export default function CreateAccount() {
 
   return (
     <Wrapper>
-      <Title>Log into 𝕏</Title>
+      <Title>Join 𝕏</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
@@ -109,7 +127,10 @@ export default function CreateAccount() {
           type="password"
           required
         />
-        <Input type="submit" value={isLoading ? "loading" : "Create Account"} />
+        <Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Create Account"}
+        />
       </Form>
       {error !== "" ? <Error>{error}</Error> : null}
     </Wrapper>
