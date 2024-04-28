2b352847b4e94e6d1c779b4faac1fc8d07906d98
이영우 <zestlee1106@gmail.com>
2024-04-28 14:01:05 +0900

    트윗 및 이미지 삭제 기능 추가
    - 이미지는 이미지 참조를 가져와서 deleteObject 해주면 끝남

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
+import { auth, db, storage } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 
 const Wrapper = styled.div`
   display: grid;
@@ -27,12 +30,48 @@ const Payload = styled.p`
   font-size: 18px;
 `;
 
-export default function Tweet({ username, photo, tweet }: ITweet) {
+const DeleteButton = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
+  const user = auth.currentUser;
+  const onDelete = async () => {
+    const ok = confirm("정말 삭제하시겠습니까?");
+
+    if (!ok || user?.uid !== userId) {
+      return;
+    }
+
+    try {
+      await deleteDoc(doc(db, "tweets", id));
+
+      if (photo) {
+        const photoRef = ref(storage, `tweets/${userId}/${id}`);
+        await deleteObject(photoRef);
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      console.log("Tweet deleted");
+    }
+  };
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
+        {user?.uid === userId ? (
+          <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+        ) : null}
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
