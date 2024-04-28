1ee0a9ef2ba24de14d608dae7ec5743da1dcd7e1
이영우 <ywlee@hanatour.com>
2024-04-28 13:41:56 +0900

    실시간으로 목록 가져오게끔 추가
    - firebase 의 onSnapshot 함수를 사용하여 실시간으로 목록을 가져오게끔 한다
    - onSnapshot 의 콜백함수로 snapshot 객체를 받아올 수 있다
    - 해당 기능은 HTTP2 long-polling 방식을 사용해서 한다
    - long-polling 방식은 클라이언트가 서버에 연결을 맺고, 서버가 데이터를 보낼 준비가 될 때까지 연결을 유지하는 방식이다
    - 따라서 현재로서는 페이지가 맨 첨에 서버한테 요청을 하고 firebase 서버는 데이터가 바뀌기 전까지 기다리다가 데이터가 바뀌면 응답을 하고, 다시 페이지가 서버한테 요청하는 방식인 것이다

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,8 +1,15 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { db } from "../firebase";
 import Tweet from "./tweet";
+import { Unsubscribe } from "firebase/auth";
 
 export interface ITweet {
   id: string;
@@ -13,32 +20,43 @@ export interface ITweet {
   createdAt: number;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+`;
 
 export default function Timeline() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, "tweets"),
-      orderBy("createdAt", "desc")
-    );
-    const snapshot = await getDocs(tweetsQuery);
-    const tweets = snapshot.docs.map((doc) => {
-      const { tweet, createdAt, userId, username, photo } =
-        doc.data() as ITweet;
-      return {
-        tweet,
-        createdAt,
-        userId,
-        username,
-        photo,
-        id: doc.id,
-      };
-    });
-    setTweets(tweets);
-  };
+
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      );
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, userId, username, photo } =
+            doc.data() as ITweet;
+          return {
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweets(tweets);
+      });
+    };
     fetchTweets();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
   return (
     <Wrapper>
