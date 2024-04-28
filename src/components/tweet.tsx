70e7f07429fbf0e126c00bdc6ec8ec24ef869b61
이영우 <zestlee1106@gmail.com>
2024-04-28 14:11:42 +0900

    트위터 수정 기능 추가
    - updateDoc 함수 이용하여 수정 기능을 추가한다
    - doc 함수로 document 를 가져오고, 두번째 인자로 수정할 내용을 추가

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 import { ITweet } from "./timeline";
 import { auth, db, storage } from "../firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   display: grid;
@@ -42,6 +43,43 @@ const DeleteButton = styled.button`
   cursor: pointer;
 `;
 
+const EditButton = styled.button`
+  background-color: skyblue;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-right: 5px;
+`;
+
+const EditTextarea = styled.textarea`
+  margin: 10px 0px;
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
+      Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+      sans-serif;
+  }
+  &:focus {
+    outline: none;
+    border-color: skyblue;
+  }
+`;
+
 export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
   const user = auth.currentUser;
   const onDelete = async () => {
@@ -64,11 +102,39 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
       console.log("Tweet deleted");
     }
   };
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTweet, setEditedTweet] = useState(tweet);
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEditedTweet(e.target.value);
+  };
+  const onEdit = async () => {
+    setIsEditing(true);
+  };
+  const onSave = async () => {
+    await updateDoc(doc(db, "tweets", id), {
+      tweet: editedTweet,
+    });
+    setIsEditing(false);
+  };
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
-        <Payload>{tweet}</Payload>
+
+        {isEditing ? (
+          <EditTextarea
+            placeholder="Edit your tweet"
+            onChange={onChange}
+            value={editedTweet}
+          />
+        ) : (
+          <Payload>{tweet}</Payload>
+        )}
+        {user?.uid === userId && !isEditing ? (
+          <EditButton onClick={onEdit}>Edit</EditButton>
+        ) : null}
+        {isEditing ? <EditButton onClick={onSave}>Save</EditButton> : null}
         {user?.uid === userId ? (
           <DeleteButton onClick={onDelete}>Delete</DeleteButton>
         ) : null}
